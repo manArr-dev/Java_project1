48d909e6bb1ee61dabc7cd0765deb9771c5c29e1
manArr-dev <84402072+manArr-dev@users.noreply.github.com>
2021-05-27 17:14:15 -0500

    first attempt

---
+++ HomeTour/Command.java
@@ -0,0 +1,74 @@
+public class Command {
+	private String commandWord;
+
+    private String secondWord;
+
+   
+
+    public Command(String firstWord, String secondWord)
+
+    {
+
+        commandWord = firstWord;
+
+        this.secondWord = secondWord;
+
+    }
+
+    public String getCommandWord()
+
+    {
+
+        return commandWord;
+
+    }
+
+    public String getSecondWord()
+
+    {
+
+        return secondWord;
+
+    }
+
+    /**
+
+     * Returns true if the command is unknown.
+
+     *
+
+     * @return true if this command was not
+
+     * understood, false otherwise
+
+     */
+
+    public boolean isUnknown()
+
+    {
+
+        return (commandWord == null);
+
+    }
+
+    /**
+
+     * Returns true if the command has a second word.
+
+     *
+
+     * @return true if the command has a second word,
+
+     * false otherwise
+
+     */
+
+    public boolean hasSecondWord()
+
+    {
+
+        return (secondWord != null);
+
+    }
+
+}
